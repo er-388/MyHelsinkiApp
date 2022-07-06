1e9726a7c003cbd7770ddbf87f06b6b0a6c99e18
Erkki Rasi <erkkirasi@gmail.com>
2022-07-06 13:19:19 +0300

    changed Event, Activity and Place classes

--- MyHelsinkiApp/Place.cs
+++ MyHelsinkiApp/Place.cs
@@ -1,8 +1,283 @@
 ﻿using System;
+namespace MyHelsinkiApp
+{
 
-namespace PlacePlace
-{ 
-    public class Place
+    public class Rootobject
+    {
+        public Meta meta { get; set; }
+        public Datum[] data { get; set; }
+        public Tags tags { get; set; }
+    }
+
+    public class Meta
+    {
+        public string count { get; set; }
+    }
+
+    public class Tags
+    {
+        public string main1012 { get; set; }
+        public string main147 { get; set; }
+        public string main197 { get; set; }
+        public string main214 { get; set; }
+        public string main239 { get; set; }
+        public string main256 { get; set; }
+        public string main326 { get; set; }
+        public string main329 { get; set; }
+        public string main344 { get; set; }
+        public string main345 { get; set; }
+        public string main350 { get; set; }
+        public string main400 { get; set; }
+        public string main459 { get; set; }
+        public string main468 { get; set; }
+        public string main473 { get; set; }
+        public string main474 { get; set; }
+        public string main490 { get; set; }
+        public string main577 { get; set; }
+        public string main587 { get; set; }
+        public string main624 { get; set; }
+        public string main647 { get; set; }
+        public string main654 { get; set; }
+        public string main686 { get; set; }
+        public string main737 { get; set; }
+        public string main783 { get; set; }
+        public string main804 { get; set; }
+        public string main812 { get; set; }
+        public string main845 { get; set; }
+        public string main85 { get; set; }
+        public string main864 { get; set; }
+        public string main868 { get; set; }
+        public string main869 { get; set; }
+        public string main884 { get; set; }
+        public string main895 { get; set; }
+        public string main896 { get; set; }
+        public string main901 { get; set; }
+        public string main914 { get; set; }
+        public string main915 { get; set; }
+        public string main916 { get; set; }
+        public string main917 { get; set; }
+        public string main918 { get; set; }
+        public string main919 { get; set; }
+        public string main920 { get; set; }
+        public string main921 { get; set; }
+        public string main922 { get; set; }
+        public string main923 { get; set; }
+        public string main924 { get; set; }
+        public string main928 { get; set; }
+        public string main929 { get; set; }
+        public string main935 { get; set; }
+        public string main938 { get; set; }
+        public string main944 { get; set; }
+        public string main961 { get; set; }
+        public string main964 { get; set; }
+        public string main967 { get; set; }
+        public string main973 { get; set; }
+        public string sub100 { get; set; }
+        public string sub101 { get; set; }
+        public string sub102 { get; set; }
+        public string sub103 { get; set; }
+        public string sub104 { get; set; }
+        public string sub105 { get; set; }
+        public string sub106 { get; set; }
+        public string sub108 { get; set; }
+        public string sub11 { get; set; }
+        public string sub110 { get; set; }
+        public string sub112 { get; set; }
+        public string sub117 { get; set; }
+        public string sub12 { get; set; }
+        public string sub122 { get; set; }
+        public string sub126 { get; set; }
+        public string sub127 { get; set; }
+        public string sub128 { get; set; }
+        public string sub129 { get; set; }
+        public string sub13 { get; set; }
+        public string sub130 { get; set; }
+        public string sub131 { get; set; }
+        public string sub132 { get; set; }
+        public string sub133 { get; set; }
+        public string sub141 { get; set; }
+        public string sub142 { get; set; }
+        public string sub144 { get; set; }
+        public string sub145 { get; set; }
+        public string sub146 { get; set; }
+        public string sub147 { get; set; }
+        public string sub148 { get; set; }
+        public string sub152 { get; set; }
+        public string sub157 { get; set; }
+        public string sub163 { get; set; }
+        public string sub166 { get; set; }
+        public string sub167 { get; set; }
+        public string sub168 { get; set; }
+        public string sub17 { get; set; }
+        public string sub170 { get; set; }
+        public string sub171 { get; set; }
+        public string sub172 { get; set; }
+        public string sub173 { get; set; }
+        public string sub174 { get; set; }
+        public string sub175 { get; set; }
+        public string sub177 { get; set; }
+        public string sub178 { get; set; }
+        public string sub179 { get; set; }
+        public string sub180 { get; set; }
+        public string sub181 { get; set; }
+        public string sub182 { get; set; }
+        public string sub183 { get; set; }
+        public string sub184 { get; set; }
+        public string sub186 { get; set; }
+        public string sub187 { get; set; }
+        public string sub188 { get; set; }
+        public string sub189 { get; set; }
+        public string sub19 { get; set; }
+        public string sub190 { get; set; }
+        public string sub191 { get; set; }
+        public string sub192 { get; set; }
+        public string sub193 { get; set; }
+        public string sub194 { get; set; }
+        public string sub195 { get; set; }
+        public string sub196 { get; set; }
+        public string sub197 { get; set; }
+        public string sub198 { get; set; }
+        public string sub199 { get; set; }
+        public string sub20 { get; set; }
+        public string sub200 { get; set; }
+        public string sub201 { get; set; }
+        public string sub203 { get; set; }
+        public string sub205 { get; set; }
+        public string sub207 { get; set; }
+        public string sub208 { get; set; }
+        public string sub209 { get; set; }
+        public string sub21 { get; set; }
+        public string sub210 { get; set; }
+        public string sub211 { get; set; }
+        public string sub212 { get; set; }
+        public string sub213 { get; set; }
+        public string sub214 { get; set; }
+        public string sub215 { get; set; }
+        public string sub216 { get; set; }
+        public string sub217 { get; set; }
+        public string sub218 { get; set; }
+        public string sub220 { get; set; }
+        public string sub223 { get; set; }
+        public string sub224 { get; set; }
+        public string sub227 { get; set; }
+        public string sub23 { get; set; }
+        public string sub230 { get; set; }
+        public string sub231 { get; set; }
+        public string sub233 { get; set; }
+        public string sub234 { get; set; }
+        public string sub235 { get; set; }
+        public string sub236 { get; set; }
+        public string sub237 { get; set; }
+        public string sub238 { get; set; }
+        public string sub239 { get; set; }
+        public string sub24 { get; set; }
+        public string sub240 { get; set; }
+        public string sub241 { get; set; }
+        public string sub242 { get; set; }
+        public string sub244 { get; set; }
+        public string sub245 { get; set; }
+        public string sub246 { get; set; }
+        public string sub249 { get; set; }
+        public string sub25 { get; set; }
+        public string sub250 { get; set; }
+        public string sub251 { get; set; }
+        public string sub252 { get; set; }
+        public string sub253 { get; set; }
+        public string sub254 { get; set; }
+        public string sub255 { get; set; }
+        public string sub257 { get; set; }
+        public string sub258 { get; set; }
+        public string sub259 { get; set; }
+        public string sub26 { get; set; }
+        public string sub260 { get; set; }
+        public string sub261 { get; set; }
+        public string sub263 { get; set; }
+        public string sub264 { get; set; }
+        public string sub265 { get; set; }
+        public string sub266 { get; set; }
+        public string sub267 { get; set; }
+        public string sub268 { get; set; }
+        public string sub269 { get; set; }
+        public string sub27 { get; set; }
+        public string sub270 { get; set; }
+        public string sub271 { get; set; }
+        public string sub272 { get; set; }
+        public string sub274 { get; set; }
+        public string sub275 { get; set; }
+        public string sub276 { get; set; }
+        public string sub278 { get; set; }
+        public string sub279 { get; set; }
+        public string sub28 { get; set; }
+        public string sub280 { get; set; }
+        public string sub281 { get; set; }
+        public string sub282 { get; set; }
+        public string sub283 { get; set; }
+        public string sub284 { get; set; }
+        public string sub287 { get; set; }
+        public string sub29 { get; set; }
+        public string sub292 { get; set; }
+        public string sub293 { get; set; }
+        public string sub294 { get; set; }
+        public string sub296 { get; set; }
+        public string sub297 { get; set; }
+        public string sub298 { get; set; }
+        public string sub299 { get; set; }
+        public string sub30 { get; set; }
+        public string sub300 { get; set; }
+        public string sub301 { get; set; }
+        public string sub303 { get; set; }
+        public string sub305 { get; set; }
+        public string sub307 { get; set; }
+        public string sub31 { get; set; }
+        public string sub32 { get; set; }
+        public string sub33 { get; set; }
+        public string sub35 { get; set; }
+        public string sub36 { get; set; }
+        public string sub38 { get; set; }
+        public string sub41 { get; set; }
+        public string sub42 { get; set; }
+        public string sub44 { get; set; }
+        public string sub46 { get; set; }
+        public string sub47 { get; set; }
+        public string sub49 { get; set; }
+        public string sub50 { get; set; }
+        public string sub52 { get; set; }
+        public string sub53 { get; set; }
+        public string sub54 { get; set; }
+        public string sub55 { get; set; }
+        public string sub59 { get; set; }
+        public string sub60 { get; set; }
+        public string sub61 { get; set; }
+        public string sub62 { get; set; }
+        public string sub65 { get; set; }
+        public string sub66 { get; set; }
+        public string sub67 { get; set; }
+        public string sub68 { get; set; }
+        public string sub69 { get; set; }
+        public string sub70 { get; set; }
+        public string sub71 { get; set; }
+        public string sub72 { get; set; }
+        public string sub76 { get; set; }
+        public string sub77 { get; set; }
+        public string sub80 { get; set; }
+        public string sub82 { get; set; }
+        public string sub83 { get; set; }
+        public string sub84 { get; set; }
+        public string sub86 { get; set; }
+        public string sub87 { get; set; }
+        public string sub88 { get; set; }
+        public string sub89 { get; set; }
+        public string sub90 { get; set; }
+        public string sub91 { get; set; }
+        public string sub92 { get; set; }
+        public string sub93 { get; set; }
+        public string sub94 { get; set; }
+        public string sub95 { get; set; }
+        public string sub96 { get; set; }
+        public string sub98 { get; set; }
+    }
+
+    public class Datum
     {
         public string id { get; set; }
         public Name name { get; set; }
@@ -12,7 +287,7 @@ namespace PlacePlace
         public Location location { get; set; }
         public Description description { get; set; }
         public Tag[] tags { get; set; }
-        public object[] extra_searchwords { get; set; }
+        public string[] extra_searchwords { get; set; }
         public string opening_hours_url { get; set; }
     }
 
@@ -71,4 +346,5 @@ namespace PlacePlace
         public string id { get; set; }
         public string name { get; set; }
     }
+
 }
